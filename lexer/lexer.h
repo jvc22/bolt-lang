bfb8f1e6fc9d8f24a3d4ecb9ef2640f48cc4f2ee
jvc22 <joaovictorcostadasilva@outlook.com>
2024-11-03 18:52:23 -0300

    init: define structures of token and lexer

---
+++ lexer/lexer.h
@@ -0,0 +1,40 @@
+#ifndef LEXER_H
+#define LEXER_H
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+enum class TokenType {
+    Undefined,
+    Identifier,
+    Number,
+    String,
+    Keyword,
+    Operator,
+    Delimiter,
+    EndOfFile
+};
+
+struct Token {
+    TokenType type;
+    string value;
+    int line;
+
+    Token(TokenType t, const string& v, int l) : type(t), value(v), line(l) {}
+};
+
+class Lexer {
+    private:
+        string source;
+        size_t position = 0;
+        int currentLine = 1;
+
+    public:
+        Lexer(const string& src);
+        Token nextToken();
+        vector<Token> tokenize();
+};
+
+#endif
